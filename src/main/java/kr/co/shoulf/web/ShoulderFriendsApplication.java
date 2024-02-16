d870bbb99cec15797407100f5db1e4eed54d6251
haeseok <haeseok_@naver.com>
2024-02-16 15:47:43 +0900

    프로젝트 생성

---
+++ src/main/java/kr/co/shoulf/web/ShoulderFriendsApplication.java
@@ -0,0 +1,13 @@
+package kr.co.shoulf.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoulderFriendsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoulderFriendsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/shoulf/web/ShoulderFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.shoulf.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoulderFriendsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
