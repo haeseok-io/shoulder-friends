e7966e601ad3311def09c7c03d7aadc193ba4aed
haeseok <haeseok_@naver.com>
2024-02-19 15:12:12 +0900

    ReplyRepository 생성

---
+++ src/main/java/kr/co/shoulf/web/repository/ReplyRepository.java
@@ -0,0 +1,9 @@
+package kr.co.shoulf.web.repository;
+
+import kr.co.shoulf.web.entity.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+}
