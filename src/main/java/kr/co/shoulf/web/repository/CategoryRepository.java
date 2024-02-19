5a9242abd742aeff6599f0583fbcdc3ce5c694e5
SJE <wodms1102@gmail.com>
2024-02-19 15:22:02 +0900

    CategoryRepository, MoimHeadcountRepository, MoimParticipantsRejectRepository, MoimParticipantsRepository, OnlineRepository, PositionDetailRepository, PositionRepository, StudyCategoryRepository, UserInterestCategoryRepository, UserJobRepository, UserOnlineRepository @ 추가

--- src/main/java/kr/co/shoulf/web/repository/CategoryRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package kr.co.shoulf.web.repository;
 
 import kr.co.shoulf.web.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
--- src/main/java/kr/co/shoulf/web/repository/MoimParticipantsRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MoimParticipantsRepository.java
@@ -3,6 +3,8 @@ package kr.co.shoulf.web.repository;
 import kr.co.shoulf.web.entity.Category;
 import kr.co.shoulf.web.entity.MoimParticipants;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MoimParticipantsRepository extends JpaRepository<MoimParticipants,Long> {
 }
