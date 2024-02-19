c42d726bd261aa3001c4078a7896aa3d7eb1a80a
TY <gkfnglfn@gmail.com>
2024-02-19 15:21:40 +0900

    MemberAnnualDetailRepository, MemberAnnualRejectedRepository, MemberAnnualRepository 생성, MemberCommuteRepository 어노테이션 수정

--- src/main/java/kr/co/shoulf/web/repository/MemberAnnualRejectedRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MemberAnnualRejectedRepository.java
@@ -1,9 +1,9 @@
 package kr.co.shoulf.web.repository;
 
-import kr.co.shoulf.web.entity.MemberCommute;
+import kr.co.shoulf.web.entity.MemberAnnualRejected;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberCommuteRepository extends JpaRepository<MemberCommute, Long> {
+public interface MemberAnnualRejectedRepository extends JpaRepository<MemberAnnualRejected, Long> {
 }
--- src/main/java/kr/co/shoulf/web/repository/MemberAnnualRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MemberAnnualRepository.java
@@ -1,9 +1,9 @@
 package kr.co.shoulf.web.repository;
 
-import kr.co.shoulf.web.entity.MemberCommute;
+import kr.co.shoulf.web.entity.MemberAnnual;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberCommuteRepository extends JpaRepository<MemberCommute, Long> {
+public interface MemberAnnualRepository extends JpaRepository<MemberAnnual, Long> {
 }
