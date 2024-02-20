857ae305a12389ff3b1f940a909c98db2930ad9c
YSM <ysm5545@naver.com>
2024-02-20 10:37:22 +0900

    불필요한 import 제거

--- src/main/java/kr/co/shoulf/web/repository/MoimRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MoimRepository.java
@@ -1,6 +1,5 @@
 package kr.co.shoulf.web.repository;
 
-import kr.co.shoulf.web.entity.Meeting;
 import kr.co.shoulf.web.entity.Moim;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
