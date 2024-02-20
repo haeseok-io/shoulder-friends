d6914a62ce781981e8a9d8bee2bf89fbc21ef027
YSM <ysm5545@naver.com>
2024-02-20 14:36:29 +0900

    미팅 샘플데이터 추가

---
+++ src/test/java/kr/co/shoulf/web/repository/MeetingRepositoryTest.java
@@ -0,0 +1,31 @@
+package kr.co.shoulf.web.repository;
+
+import kr.co.shoulf.web.entity.Meeting;
+import kr.co.shoulf.web.entity.Moim;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class MeetingRepositoryTest {
+    @Autowired
+    MeetingRepository meetingRepository;
+    @Autowired
+    MoimRepository  moimRepository;
+
+    @Test
+    void insertData(){
+        Optional<Moim> moim = moimRepository.findById(1L);
+        Meeting meeting = Meeting.builder()
+                .meetDate(LocalDateTime.parse("2024-03-20T14:30:00"))
+                .addr("서울 종로구")
+                .contents("회의")
+                .moim(moim.get())
+                .build();
+        meetingRepository.save(meeting);
+    }
+}
