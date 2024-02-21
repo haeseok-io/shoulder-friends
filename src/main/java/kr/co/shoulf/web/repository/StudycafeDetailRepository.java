7888a8608214bca3826e8b8b99ee508b12635410
TY <gkfnglfn@gmail.com>
2024-02-21 12:17:54 +0900

    StudycafeDetail 엔티티 삭제

--- src/main/java/kr/co/shoulf/web/repository/StudycafeDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.co.shoulf.web.repository;
-
-import kr.co.shoulf.web.entity.StudycafeDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudycafeDetailRepository extends JpaRepository<StudycafeDetail, Long> {
-}
