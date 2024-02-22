c1fe5986be6d7f521c13148512eaf2b6e436eb77
YSM <ysm5545@naver.com>
2024-02-22 10:25:37 +0900

    MoimSerivceImple 생성

---
+++ src/main/java/kr/co/shoulf/web/service/MoimSerivceImple.java
@@ -0,0 +1,23 @@
+package kr.co.shoulf.web.service;
+
+import jakarta.transaction.Transactional;
+import kr.co.shoulf.web.entity.MoimDetail;
+import kr.co.shoulf.web.repository.MoimDetailRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class MoimSerivceImple implements MoimService{
+    private final MoimDetailRepository moimDetailRepository;
+
+    @Override
+    public List<MoimDetail> getList() {
+        return moimDetailRepository.findAll().stream().toList();
+    }
+}
