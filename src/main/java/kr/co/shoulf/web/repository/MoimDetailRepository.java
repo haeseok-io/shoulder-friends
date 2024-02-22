b7fe55461c0711a426a1b6be5f7fd1e9b7e55f4f
YSM <ysm5545@naver.com>
2024-02-22 12:21:32 +0900

    MoimDetailRepository 생성

--- src/main/java/kr/co/shoulf/web/repository/MoimDetailRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MoimDetailRepository.java
@@ -2,8 +2,17 @@ package kr.co.shoulf.web.repository;
 
 import kr.co.shoulf.web.entity.MoimDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MoimDetailRepository extends JpaRepository<MoimDetail,Long> {
+
+    @Query(value = "SELECT * FROM moim_detail  m order by m.moim_no desc limit :limit", nativeQuery = true)
+    List<MoimDetail> readNewMoim(@Param("limit")int limit);
+
+    List<MoimDetail> findTop2ByOrderByMoimNoDesc();
 }
