ca98a2e7a99d4c13ef647d9c3a63b4dae7b234b9
YSM <ysm5545@naver.com>
2024-02-22 10:25:57 +0900

    @RequestMapping("/moim") 추가

--- src/main/java/kr/co/shoulf/web/control/MoimController.java
+++ src/main/java/kr/co/shoulf/web/control/MoimController.java
@@ -1,26 +1,27 @@
 package kr.co.shoulf.web.control;
 
-import kr.co.shoulf.web.entity.Moim;
 import kr.co.shoulf.web.entity.MoimDetail;
 import kr.co.shoulf.web.repository.MoimDetailRepository;
-import kr.co.shoulf.web.repository.MoimRepository;
+import kr.co.shoulf.web.service.MoimService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/moim")
 public class MoimController {
-    private final MoimRepository moimRepository;
+    private final MoimService moimService;
 
-    @GetMapping("/moim")
+    @GetMapping("/list")
     public String moimList(Model model){
-        List<Moim> moimList = moimRepository.findAll().stream().toList();
+        List<MoimDetail> moimList = moimService.getList();
         model.addAttribute("moimList", moimList);
         System.out.println(moimList);
-        return "moimList";
+        return "moim/list";
     }
 }
