f462fc46b14b26f94d22f8f0e57ac1dcbc36add5
haeseok <haeseok_@naver.com>
2024-02-22 10:46:50 +0900

    테스트코드 주석처리

--- src/test/java/kr/co/shoulf/web/repository/MoimLikeRepositoryTest.java
+++ src/test/java/kr/co/shoulf/web/repository/MoimLikeRepositoryTest.java
@@ -35,16 +35,16 @@ class MoimLikeRepositoryTest {
     @Test
     void insertData() {
         List<MoimLike> list = new ArrayList<>();
-        list.add(MoimLike.builder().user(usersList.get(0)).moim(moimList.get(9)).build());
-        list.add(MoimLike.builder().user(usersList.get(1)).moim(moimList.get(8)).build());
-        list.add(MoimLike.builder().user(usersList.get(2)).moim(moimList.get(7)).build());
-        list.add(MoimLike.builder().user(usersList.get(3)).moim(moimList.get(6)).build());
-        list.add(MoimLike.builder().user(usersList.get(4)).moim(moimList.get(5)).build());
-        list.add(MoimLike.builder().user(usersList.get(5)).moim(moimList.get(4)).build());
-        list.add(MoimLike.builder().user(usersList.get(6)).moim(moimList.get(3)).build());
-        list.add(MoimLike.builder().user(usersList.get(7)).moim(moimList.get(2)).build());
-        list.add(MoimLike.builder().user(usersList.get(8)).moim(moimList.get(1)).build());
-        list.add(MoimLike.builder().user(usersList.get(9)).moim(moimList.get(0)).build());
-        list.forEach(moimLike -> moimLikeRepository.save(moimLike));
+        //list.add(MoimLike.builder().user(usersList.get(0)).moim(moimList.get(9)).build());
+        //list.add(MoimLike.builder().user(usersList.get(1)).moim(moimList.get(8)).build());
+        //list.add(MoimLike.builder().user(usersList.get(2)).moim(moimList.get(7)).build());
+        //list.add(MoimLike.builder().user(usersList.get(3)).moim(moimList.get(6)).build());
+        //list.add(MoimLike.builder().user(usersList.get(4)).moim(moimList.get(5)).build());
+        //list.add(MoimLike.builder().user(usersList.get(5)).moim(moimList.get(4)).build());
+        //list.add(MoimLike.builder().user(usersList.get(6)).moim(moimList.get(3)).build());
+        //list.add(MoimLike.builder().user(usersList.get(7)).moim(moimList.get(2)).build());
+        //list.add(MoimLike.builder().user(usersList.get(8)).moim(moimList.get(1)).build());
+        //list.add(MoimLike.builder().user(usersList.get(9)).moim(moimList.get(0)).build());
+        //list.forEach(moimLike -> moimLikeRepository.save(moimLike));
     }
 }
