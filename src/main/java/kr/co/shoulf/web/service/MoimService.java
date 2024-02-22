ff274f97c121ccb91197c76c233a4a5b737e6fbf
YSM <ysm5545@naver.com>
2024-02-22 10:25:30 +0900

    MoimService 생성

---
+++ src/main/java/kr/co/shoulf/web/service/MoimService.java
@@ -0,0 +1,10 @@
+package kr.co.shoulf.web.service;
+
+import kr.co.shoulf.web.entity.MoimDetail;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface MoimService {
+    List<MoimDetail> getList();
+}
