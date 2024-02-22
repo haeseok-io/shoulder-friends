d9713128b34c6cf10ce696cc82077ef77955aca5
YSM <ysm5545@naver.com>
2024-02-22 17:45:57 +0900

    List<Moim> findTop2ByOrderByMoimNoDesc() 메서드 생성

--- src/main/java/kr/co/shoulf/web/repository/MoimRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MoimRepository.java
@@ -4,6 +4,9 @@ import kr.co.shoulf.web.entity.Moim;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MoimRepository extends JpaRepository<Moim,Long> {
+    List<Moim> findTop2ByOrderByMoimNoDesc();
 }
