5c848f01369f457054c3f623a348a921d36437dc
haeseok <haeseok_@naver.com>
2024-02-23 12:36:44 +0900

    - 회원, 상태값을 받아 갯수 리턴해주는 메서드 추가

--- src/main/java/kr/co/shoulf/web/repository/MoimParticipantsRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MoimParticipantsRepository.java
@@ -2,6 +2,7 @@ package kr.co.shoulf.web.repository;
 
 import kr.co.shoulf.web.entity.MoimHeadcount;
 import kr.co.shoulf.web.entity.MoimParticipants;
+import kr.co.shoulf.web.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import java.util.List;
 @Repository
 public interface MoimParticipantsRepository extends JpaRepository<MoimParticipants, Long> {
     Integer countByMoimHeadcountAndStatus(MoimHeadcount moimHeadcount, Integer status);
+    Integer countByUsersAndStatus(Users users, Integer status);
 }
