0f3dc3d76ff080138a526947ccabca796a4a0ab6
haeseok <haeseok_@naver.com>
2024-02-23 12:22:11 +0900

    - 지원한 참가자들의 상태값 기준으로 인원수 가져오는 메서드 추가

--- src/main/java/kr/co/shoulf/web/repository/MoimParticipantsRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MoimParticipantsRepository.java
@@ -1,10 +1,13 @@
 package kr.co.shoulf.web.repository;
 
-import kr.co.shoulf.web.entity.Category;
+import kr.co.shoulf.web.entity.MoimHeadcount;
 import kr.co.shoulf.web.entity.MoimParticipants;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface MoimParticipantsRepository extends JpaRepository<MoimParticipants,Long> {
+public interface MoimParticipantsRepository extends JpaRepository<MoimParticipants, Long> {
+    Integer countByMoimHeadcountAndStatus(MoimHeadcount moimHeadcount, Integer status);
 }
