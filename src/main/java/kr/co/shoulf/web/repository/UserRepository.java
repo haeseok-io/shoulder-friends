3bc8555ba2e4448a06da4d0082fa16358b3213e0
haeseok <haeseok_@naver.com>
2024-02-23 12:39:11 +0900

    - 최근 등록된 12명의 회원정보를 가져오는 메서드 추가

--- src/main/java/kr/co/shoulf/web/repository/UserRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/UserRepository.java
@@ -4,6 +4,9 @@ import kr.co.shoulf.web.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
+    List<Users> findTop12ByOrderByUserNoDesc();
 }
