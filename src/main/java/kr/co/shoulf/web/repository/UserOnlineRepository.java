3fc0a908ad7c8b541947ee927072fee60398bb25
haeseok <haeseok_@naver.com>
2024-02-23 12:38:44 +0900

    - 회원의 온/오프라인 정보를 가져오는 메서드 추가

--- src/main/java/kr/co/shoulf/web/repository/UserOnlineRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/UserOnlineRepository.java
@@ -2,9 +2,11 @@ package kr.co.shoulf.web.repository;
 
 import kr.co.shoulf.web.entity.Category;
 import kr.co.shoulf.web.entity.UserOnline;
+import kr.co.shoulf.web.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserOnlineRepository extends JpaRepository<UserOnline,Long> {
+    UserOnline findByUsers(Users users);
 }
