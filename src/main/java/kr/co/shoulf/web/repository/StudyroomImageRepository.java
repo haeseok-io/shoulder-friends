6869e83a7e91fdac7a04a1f1e83a516875cb710f
SJE <wodms1102@gmail.com>
2024-02-23 17:42:59 +0900

    이미지 목록 찾기 추가

--- src/main/java/kr/co/shoulf/web/repository/StudyroomImageRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/StudyroomImageRepository.java
@@ -1,9 +1,14 @@
 package kr.co.shoulf.web.repository;
 
+import kr.co.shoulf.web.entity.Studyroom;
 import kr.co.shoulf.web.entity.StudyroomImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudyroomImageRepository extends JpaRepository<StudyroomImage, Long> {
+    //스터디룸 번호로 이미지 목록
+    List<StudyroomImage> findByStudyroom(Studyroom studyroom);
 }
