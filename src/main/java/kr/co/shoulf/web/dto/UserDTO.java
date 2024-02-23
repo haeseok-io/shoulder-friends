30d8f5f37b1f357b2b6259bef64eaae53d87f7a0
haeseok <haeseok_@naver.com>
2024-02-23 12:36:55 +0900

    - 회원 DTO 추가

---
+++ src/main/java/kr/co/shoulf/web/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package kr.co.shoulf.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long userNo;
+    private String nickname;
+    private String profileImg;
+    private String introduce;
+    private Integer positionLevel;
+    private String positionDetailName;
+    private String online;
+
+    private Integer progressMoimNum;
+}
