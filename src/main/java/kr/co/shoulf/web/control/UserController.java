3cdb5bd5e0927f702cca90f22e2d543a400da272
haeseok <haeseok_@naver.com>
2024-02-26 14:21:08 +0900

    팀원 상세페이지 컨트롤, html 생성

--- src/main/java/kr/co/shoulf/web/control/UserController.java
+++ src/main/java/kr/co/shoulf/web/control/UserController.java
@@ -1,8 +1,15 @@
 package kr.co.shoulf.web.control;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/user")
 public class UserController {
+    @GetMapping(value = "/detail")
+    public String detail() {
 
+        return "user/detail";
+    }
 }
