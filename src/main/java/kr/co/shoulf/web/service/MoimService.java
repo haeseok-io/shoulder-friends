42dca2fbbc4313aa971bcc903369dc3e99c40f3f
YSM <ysm5545@naver.com>
2024-02-26 15:56:01 +0900

    public List<MoimDTO> readNewMoim() 수정

--- src/main/java/kr/co/shoulf/web/service/MoimService.java
+++ src/main/java/kr/co/shoulf/web/service/MoimService.java
@@ -1,6 +1,5 @@
 package kr.co.shoulf.web.service;
 
-import jakarta.transaction.Transactional;
 import kr.co.shoulf.web.dto.*;
 import kr.co.shoulf.web.entity.*;
 import kr.co.shoulf.web.repository.*;
@@ -30,8 +29,59 @@ public class MoimService {
         return moimRepository.findAll();
     }
 
-    public List<Moim> readNewMoim() { // 신규 모임 가져오기
-        return moimRepository.findTop2ByOrderByMoimNoDesc();
+    public List<MoimDTO> readNewMoim() { // 신규 모임 가져오기
+        List<MoimDTO> list = new ArrayList<>();
+        moimRepository.findTop2ByOrderByMoimNoDesc().forEach(moim -> {
+            // 타입
+            String type = moim.getType().equals("project") ? "프로젝트" : "스터디";
+
+            // 모임 카테고리
+            String category = moim.getMoimDetail().getCategory()!=null ? moim.getMoimDetail().getCategory().getCategoryName() : moim.getMoimDetail().getStudyCategory().getStudyCategoryName();
+
+            // 모임 사용언어/기술
+            List<LanguageDTO> languageList = new ArrayList<>();
+            moimLanguageRepository.findByMoim(moim).forEach(moimLanguage -> {
+                languageList.add(
+                        LanguageDTO.builder()
+                                .name(moimLanguage.getName())
+                                .path(LanguageImgPathConvert.getImgPath(moimLanguage.getName()))
+                                .build()
+                );
+            });
+
+            // 모집 직무/인원
+            List<HeadcountDTO> headcountList = new ArrayList<>();
+            moimHeadcountRepository.findByMoim(moim).forEach(moimHeadcount -> {
+                PositionDetail positionDetail = moimHeadcount.getPositionDetail();
+                String positionName = positionDetail!=null ? positionDetail.getPosition().getBigName() : "무관";
+                String positionDetailName = positionDetail!=null ? positionDetail.getMiddleName() : "누구나 가능";
+
+                headcountList.add(
+                        HeadcountDTO.builder()
+                                .no(moimHeadcount.getMoimHeadcountNo())
+                                .personnel(moimHeadcount.getPersonnel())
+                                .positionName(positionName)
+                                .positionDetailName(positionDetailName)
+                                .approvalNum(moimParticipantsRepository.countByMoimHeadcountAndStatus(moimHeadcount, 2))
+                                .build()
+                );
+            });
+
+            list.add(
+                    MoimDTO.builder()
+                            .no(moim.getMoimNo())
+                            .img(moim.getMoimDetail().getMoimImg())
+                            .type(type)
+                            .category(category)
+                            .subject(moim.getSubject())
+                            .desc(moim.getShortDesc())
+                            .like(moimLikeRepository.countByMoim(moim))
+                            .languageList(languageList)
+                            .headcountList(headcountList)
+                            .build()
+            );
+        });
+        return list;
     }
 
     public List<MoimDTO> readBestList() {
