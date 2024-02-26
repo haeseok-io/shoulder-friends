a9be795a7393ee00178be2dc7261f49a66d47fa3
TY <gkfnglfn@gmail.com>
2024-02-26 15:39:56 +0900

    security 권한 설정 변경

--- src/main/java/kr/co/shoulf/web/security/SecurityConfig.java
+++ src/main/java/kr/co/shoulf/web/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package kr.co.shoulf.web.security;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Slf4j
 @Configuration
 @EnableWebSecurity
+@Order(2)
 public class SecurityConfig{
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -25,7 +27,7 @@ public class SecurityConfig{
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/login/**").permitAll()
-                        .requestMatchers("/admin/**").hasAnyRole("admin","emp")                        .anyRequest().permitAll()
+                        .anyRequest().permitAll()
                 );
         http
                 .formLogin((auth) -> auth
