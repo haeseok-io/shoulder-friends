ae8b9ce0da13d180e3835e747ed066f96e199fe8
haeseok <haeseok_@naver.com>
2024-02-28 16:12:56 +0900

    메서드 변경 - 기존 : 직무 중분류 정보로 호출 - 변경 : 직무 대분류 정보 호출 -> 중분류 정보는 중분류 서비스에서 호출하도록

--- src/main/java/kr/co/shoulf/web/service/PositionService.java
+++ src/main/java/kr/co/shoulf/web/service/PositionService.java
@@ -1,33 +1,27 @@
 package kr.co.shoulf.web.service;
 
 import jakarta.transaction.Transactional;
-import kr.co.shoulf.web.entity.PositionDetail;
-import kr.co.shoulf.web.repository.PositionDetailRepository;
+import kr.co.shoulf.web.entity.Position;
+import kr.co.shoulf.web.repository.PositionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
 public class PositionService {
-    private final PositionDetailRepository positionDetailRepository;
+    private final PositionRepository positionRepository;
 
-    public List<PositionDetail> readAll() { // 직무정보 전체가져오기
-        List<PositionDetail> result = positionDetailRepository.findAll();
-
-        return result;
+    public List<Position> readAll() {
+        return positionRepository.findAll();
     }
 
-    public PositionDetail readOne(int no) {
-        Optional<PositionDetail> result = positionDetailRepository.findById(no);
-        PositionDetail positionDetail = result.orElseThrow();
-        return positionDetail;
-
+    public Position readOne(Long no) {
+        return positionRepository.findById(no).orElse(null);
     }
 }
 
