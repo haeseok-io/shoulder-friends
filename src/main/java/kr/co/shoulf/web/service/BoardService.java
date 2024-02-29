6c878a0eed01dbe1af2e75dd4fb023c906bcc018
YSM <ysm5545@naver.com>
2024-02-29 16:08:12 +0900

    읽기 기능, 저장 기능 추가

--- src/main/java/kr/co/shoulf/web/service/BoardService.java
+++ src/main/java/kr/co/shoulf/web/service/BoardService.java
@@ -1,19 +1,25 @@
 package kr.co.shoulf.web.service;
 
 import kr.co.shoulf.web.dto.BoardDTO;
+import kr.co.shoulf.web.dto.PageRequestDTO;
+import kr.co.shoulf.web.dto.PageResponseDTO;
 import kr.co.shoulf.web.entity.Board;
 import kr.co.shoulf.web.repository.BoardRepository;
 import kr.co.shoulf.web.repository.ReplyRepository;
 import kr.co.shoulf.web.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class BoardService {
     private final BoardRepository boardRepository;
     private final UserRepository userRepository;
@@ -26,18 +32,34 @@ public class BoardService {
         return boardDTO;
     }
 
-    public List<BoardDTO> readAll() {
-        List<BoardDTO> list = new ArrayList<>();
-        boardRepository.findAllByOrderByBoardNoDesc().forEach(board -> {
-            list.add(
-                    BoardDTO.builder()
-                            .board(board)
-                            .build()
-            );
-        });
-        return list;
+    public PageResponseDTO<Board> readAll(PageRequestDTO pageRequestDTO) {
+        Pageable pageable = pageRequestDTO.getPageable("boardNo");
+        log.info("pageable : {}", pageable);
+        Page<Board> result = boardRepository.findAll(pageable);
+        List<Board> list = result.getContent().stream().collect(Collectors.toList());
+
+        PageResponseDTO<Board> dto = PageResponseDTO.<Board>pageBuilder()
+                .pageRequestDTO(pageRequestDTO)
+                .dataList(list)
+                .total((int) result.getTotalElements())
+                .build();
+        return dto;
     }
 
+    public PageResponseDTO<Board> readCate(int cate , PageRequestDTO pageRequestDTO) {
+        Pageable pageable = pageRequestDTO.getPageable("boardNo");
+        log.info("pageable : {}", pageable);
+
+        Page<Board> result = boardRepository.findAllByCateEquals(cate,pageable);
+        List<Board> list = result.getContent().stream().collect(Collectors.toList());
+        PageResponseDTO<Board> dto = PageResponseDTO.<Board>pageBuilder()
+                .pageRequestDTO(pageRequestDTO)
+                .dataList(list)
+                .total((int) result.getTotalElements())
+                .build();
+        return dto;
+    }
+/*
     public List<BoardDTO> readCate(int cate) {
         List<BoardDTO> list = new ArrayList<>();
         boardRepository.findAllByCateEquals(cate).forEach(board -> {
@@ -50,6 +72,20 @@ public class BoardService {
         System.out.println(list);
         return list;
     }
+*/
+    /*
+        public List<BoardDTO> readAll() {
+            List<BoardDTO> list = new ArrayList<>();
+            boardRepository.findAllByOrderByBoardNoDesc().forEach(board -> {
+                list.add(
+                        BoardDTO.builder()
+                                .board(board)
+                                .build()
+                );
+            });
+            return list;
+        }
+    */
 
     public BoardDTO readOne(Long boardNo) {
         Optional<Board> result = boardRepository.findById(boardNo);
@@ -58,5 +94,9 @@ public class BoardService {
                 .build();
 
         return boardDTO;
+    }
+
+    public void add(Board board) {
+        boardRepository.save(board);
     }
 }
