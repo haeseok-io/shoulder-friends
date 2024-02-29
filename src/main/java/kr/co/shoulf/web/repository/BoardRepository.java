e1c19588a53a441a004b94eae99dec9ada5356f0
YSM <ysm5545@naver.com>
2024-02-29 16:07:51 +0900

    페이징 기능 추가

--- src/main/java/kr/co/shoulf/web/repository/BoardRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/BoardRepository.java
@@ -1,14 +1,13 @@
 package kr.co.shoulf.web.repository;
 
 import kr.co.shoulf.web.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Board findTopByOrderByHitsDesc();
-    List<Board> findAllByOrderByBoardNoDesc();
-    List<Board> findAllByCateEquals(int cate);
+    Page<Board> findAllByCateEquals(int cate, Pageable pageable);
 }
