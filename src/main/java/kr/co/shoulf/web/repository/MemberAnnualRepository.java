e9ac042a73f25f1a8776278f7f77a9e8806dd1a3
TY <gkfnglfn@gmail.com>
2024-03-04 16:30:46 +0900

    MemberAnnual 관련 레포지토리 수정

--- src/main/java/kr/co/shoulf/web/repository/MemberAnnualRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MemberAnnualRepository.java
@@ -4,6 +4,10 @@ import kr.co.shoulf.web.entity.MemberAnnual;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemberAnnualRepository extends JpaRepository<MemberAnnual, Long> {
+    List<MemberAnnual> findByMember_MemberNo(Long memberNo);
+    MemberAnnual findByAnnualNo(Long annualNo);
 }
