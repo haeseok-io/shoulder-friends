3eba7116dc51f7c88f984b2b6a84401055e23962
haeseok <haeseok_@naver.com>
2024-03-04 16:50:57 +0900

    모임 등록 서비스 추가

--- src/main/java/kr/co/shoulf/web/service/MoimService.java
+++ src/main/java/kr/co/shoulf/web/service/MoimService.java
@@ -6,24 +6,42 @@ import kr.co.shoulf.web.repository.*;
 import kr.co.shoulf.web.util.LanguageImgPathConvert;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MoimService {
+    private final UserRepository userRepository;
     private final MoimRepository moimRepository;
+    private final MoimDetailRepository moimDetailRepository;
     private final MoimLikeRepository moimLikeRepository;
-    private final MoimLanguageRepository moimLanguageRepository;
+    private final PlatformRepository platformRepository;
+    private final MoimProjectPlatformRepository moimProjectPlatformRepository;
     private final MoimHeadcountRepository moimHeadcountRepository;
+    private final MoimLanguageRepository moimLanguageRepository;
+    private final MoimProjectLinkRepository moimProjectLinkRepository;
     private final MoimParticipantsRepository moimParticipantsRepository;
     private final MoimParticipantsRejectRepository moimParticipantsRejectRepository;
+    private final OnlineRepository onlineRepository;
+    private final CategoryRepository categoryRepository;
+    private final StudyCategoryRepository studyCategoryRepository;
+    private final PositionDetailRepository positionDetailRepository;
+
+    @Value("${kr.co.shoulf.upload.directory}")
+    private String uploadPath;
 
     public List<Moim> readAll() {
         return null;
@@ -52,6 +70,111 @@ public class MoimService {
         return getMoimData(moimRepository.findTop8ByOrderByHitsDesc());
     }
 
+    // 모임 등록
+    public boolean addOne(MoimDataRequestDTO dto) {
+        String fileName = null;
+
+        // 임시 유저 - 추 후 로그인중인 유저로 변경해야함
+        Users user = userRepository.findById(1L).orElse(null);
+        Online online = onlineRepository.findById(dto.getOnlineNo()).orElse(null);
+        Category category = dto.getCategoryNo()==null ? null : categoryRepository.findById(dto.getCategoryNo()).orElse(null);
+        StudyCategory studyCategory = dto.getStudyCategoryNo()==null ? null : studyCategoryRepository.findById(dto.getStudyCategoryNo()).orElse(null);
+
+        // 이미지 업로드
+        if( dto.getMoimImg()!=null && !dto.getMoimImg().isEmpty() ) {
+            MultipartFile file = dto.getMoimImg();
+            String[] fileInfo = file.getOriginalFilename().split("\\.");
+            String fileSuffix = fileInfo[fileInfo.length - 1];
+            fileName = "moim_" + UUID.randomUUID().toString()+"."+fileSuffix;
+
+            Path savePath = Paths.get(uploadPath+"moim/", fileName);
+            try {
+                file.transferTo(savePath);
+            }
+            catch (IOException e) {
+                return false;
+            }
+        }
+
+        // 모임 등록
+        Moim moim = Moim.builder()
+                .type(dto.getType())
+                .subject(dto.getSubject())
+                .shortDesc(dto.getShortDesc())
+                .users(user)
+                .build();
+
+        MoimDetail moimDetail = MoimDetail.builder()
+                .detailDesc(dto.getDetailDesc())
+                .moimImg(fileName)
+                .fee(dto.getFee())
+                .offAddr(dto.getOffAddr())
+                .online(online)
+                .category(category)
+                .studyCategory(studyCategory)
+                .moim(moim)
+                .build();
+
+        MoimDetail insertData = moimDetailRepository.save(moimDetail);
+
+        // 출시 플랫폼 등록
+        if( dto.getPlatformNo()!=null && !dto.getPlatformNo().isEmpty() ) {
+            dto.getPlatformNo().forEach(platformNo -> {
+                Platform platform = platformRepository.findById(platformNo).orElse(null);
+
+                moimProjectPlatformRepository.save(
+                        MoimProjectPlatform.builder()
+                                .platform(platform)
+                                .moim(insertData.getMoim())
+                                .build()
+                );
+            });
+        }
+
+        // 모집인원 등록
+        if( dto.getPositionNo()!=null && !dto.getPositionNo().isEmpty() ) {
+            for(int i=0; i<dto.getPositionNo().size(); i++) {
+                PositionDetail positionDetail = positionDetailRepository.findById(dto.getPositionDetailNo().get(i)).orElse(null);
+
+                moimHeadcountRepository.save(
+                        MoimHeadcount.builder()
+                                .personnel(dto.getPersonnel().get(i))
+                                .moim(insertData.getMoim())
+                                .positionDetail(positionDetail)
+                                .build()
+                );
+            }
+        }
+
+        // 사용언어/기술 등록
+        if( !dto.getLanguage().isEmpty() ) {
+            dto.getLanguage().forEach(language -> {
+                moimLanguageRepository.save(
+                        MoimLanguage.builder()
+                                .moim(insertData.getMoim())
+                                .name(language)
+                                .build()
+                );
+            });
+        }
+
+        // 참고자료
+        if( dto.getLink()!=null && !dto.getLink().isEmpty() ) {
+            dto.getLink().forEach(link -> {
+                moimProjectLinkRepository.save(
+                        MoimProjectLink.builder()
+                                .moim(insertData.getMoim())
+                                .url(link)
+                                .build()
+                );
+            });
+        }
+
+
+        return true;
+    }
+
+    // 모임 데이터 DTO 로 변환
     private List<MoimDTO> getMoimData(List<Moim> moimList) {
         List<MoimDTO> list = new ArrayList<>();
 
