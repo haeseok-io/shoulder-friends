f6ac913a80f638e0a0f2560dd00023098c3dc803
SJE <wodms1102@gmail.com>
2024-03-04 17:50:45 +0900

    임시 추가

--- src/main/java/kr/co/shoulf/web/control/MeetingController.java
+++ src/main/java/kr/co/shoulf/web/control/MeetingController.java
@@ -1,7 +1,26 @@
 package kr.co.shoulf.web.control;
 
+import kr.co.shoulf.web.service.MeetingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/meeting")
 public class MeetingController {
+    private final MeetingService meetingService;
+
+    @GetMapping("/home")
+    public void home(){
+
+    }
+
+    @GetMapping("/detail")
+    public void detail(Model model, @RequestParam Long moimNo){
+        model.addAttribute("meetinglist", meetingService.meetingList(moimNo));
+    }
 }
--- src/main/java/kr/co/shoulf/web/repository/MeetingRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MeetingRepository.java
@@ -4,6 +4,9 @@ import kr.co.shoulf.web.entity.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting,Long> {
+    List<Meeting> findByMoim_MoimNo(Long moimNo);
 }
