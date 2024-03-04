1854a65438d8993a091cdde4753eb6997f194bf2
haeseok <haeseok_@naver.com>
2024-03-04 16:49:50 +0900

    모입 쓰기용 DTO 생성

---
+++ src/main/java/kr/co/shoulf/web/dto/MoimDataRequestDTO.java
@@ -0,0 +1,34 @@
+package kr.co.shoulf.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MoimDataRequestDTO {
+    private Long moimNo;
+    private String type;
+    private String subject;
+    private String shortDesc;
+    private String detailDesc;
+    private MultipartFile moimImg;
+    private Long categoryNo;
+    private Long studyCategoryNo;
+    private List<Integer> platformNo;
+    private Long onlineNo;
+    private String offAddr;
+    private Integer fee;
+    private List<Integer> headcountNo;
+    private List<Integer> positionNo;
+    private List<Integer> positionDetailNo;
+    private List<Integer> personnel;
+    private List<String> language;
+    private List<String> link;
+}
