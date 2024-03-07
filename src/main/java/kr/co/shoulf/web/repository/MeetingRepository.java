3eb1cc38f415bac3750716fda4f99f96986a5baf
SJE <wodms1102@gmail.com>
2024-03-07 17:40:31 +0900

    모임번호랑 미팅일로 미팅 찾기 추가

--- src/main/java/kr/co/shoulf/web/repository/MeetingRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MeetingRepository.java
@@ -4,9 +4,14 @@ import kr.co.shoulf.web.entity.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting,Long> {
+    //모임 번호로 미팅 리스트 찾기
     List<Meeting> findByMoim_MoimNo(Long moimNo);
+    //모임번호랑 미팅일로 미팅 찾기
+    Meeting findByMeetDateAndMoim_MoimNo(Date meetDate, Long moimNo);
+
 }
