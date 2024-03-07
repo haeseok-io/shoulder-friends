26672a20a782a08cd20e6efd184e6ab0a8e5863c
YSM <ysm5545@naver.com>
2024-03-07 17:16:16 +0900

    댓글 과 대댓글 가져오는 메서드 생성

--- src/main/java/kr/co/shoulf/web/repository/ReplyRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/ReplyRepository.java
@@ -4,6 +4,10 @@ import kr.co.shoulf.web.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    List<Reply> findByParentReplyIsNotNullAndBoard_BoardNo(Long boardNo);
+    List<Reply> findByBoard_BoardNoOrderByRegdateDesc(Long boardNo);
 }
