97ec82bd62e9b7cbf57b4f8f1d2937e3770c2bbf
haeseok <haeseok_@naver.com>
2024-03-12 09:26:55 +0900

    모임에 해당하는 링크 가져오는 메서드 추가

--- src/main/java/kr/co/shoulf/web/repository/MoimProjectLinkRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MoimProjectLinkRepository.java
@@ -1,10 +1,13 @@
 package kr.co.shoulf.web.repository;
 
-import kr.co.shoulf.web.entity.Meeting;
+import kr.co.shoulf.web.entity.Moim;
 import kr.co.shoulf.web.entity.MoimProjectLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MoimProjectLinkRepository extends JpaRepository<MoimProjectLink,Long> {
+    List<MoimProjectLink> findByMoim(Moim moim);
 }
