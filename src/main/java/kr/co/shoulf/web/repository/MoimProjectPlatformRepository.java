fc8a4969c9dff715681f4887e937da7d89f53335
haeseok <haeseok_@naver.com>
2024-03-12 09:27:06 +0900

    모임에 해당하는 플랫폼 가져오는 메서드 추가

--- src/main/java/kr/co/shoulf/web/repository/MoimProjectPlatformRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MoimProjectPlatformRepository.java
@@ -1,10 +1,13 @@
 package kr.co.shoulf.web.repository;
 
-import kr.co.shoulf.web.entity.Meeting;
+import kr.co.shoulf.web.entity.Moim;
 import kr.co.shoulf.web.entity.MoimProjectPlatform;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MoimProjectPlatformRepository extends JpaRepository<MoimProjectPlatform,Long> {
+    List<MoimProjectPlatform> findByMoim(Moim moim);
 }
