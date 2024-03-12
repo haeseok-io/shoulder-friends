6108cbb0172a269edd4787421a6bf83269662de3
haeseok <haeseok_@naver.com>
2024-03-12 09:28:20 +0900

    import 수정

--- src/main/java/kr/co/shoulf/web/entity/Users.java
+++ src/main/java/kr/co/shoulf/web/entity/Users.java
@@ -2,7 +2,6 @@ package kr.co.shoulf.web.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
