a8e256276a0724f1123db1ffda913186fde9c813
YSM <ysm5545@naver.com>
2024-03-12 17:43:33 +0900

    findReplyWithChildrenByReplyNo 메서드 추가

--- src/main/java/kr/co/shoulf/web/repository/ReplyRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/ReplyRepository.java
@@ -2,12 +2,17 @@ package kr.co.shoulf.web.repository;
 
 import kr.co.shoulf.web.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
     List<Reply> findByParentReplyIsNotNullAndBoard_BoardNo(Long boardNo);
     List<Reply> findByBoard_BoardNoOrderByRegdateDesc(Long boardNo);
+    @Query("SELECT r FROM Reply r JOIN FETCH r.children WHERE r.replyNo = :replyNo")
+    List<Reply> findReplyWithChildrenByReplyNo(@Param("replyNo") Long replyNo);
 }
