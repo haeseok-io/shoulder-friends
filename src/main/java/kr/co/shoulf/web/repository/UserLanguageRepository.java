bfacfbf8e87ea5b9cd18baa7b90b9b2f05943539
haeseok <haeseok_@naver.com>
2024-03-13 15:04:33 +0900

    유저 엔티티로 유저 사용언어/기술 조회 메서드 추가

--- src/main/java/kr/co/shoulf/web/repository/UserLanguageRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/UserLanguageRepository.java
@@ -1,9 +1,13 @@
 package kr.co.shoulf.web.repository;
 
 import kr.co.shoulf.web.entity.UserLanguage;
+import kr.co.shoulf.web.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserLanguageRepository extends JpaRepository<UserLanguage, Long> {
+    List<UserLanguage> findByUsers(Users users);
 }
