c416f708a32f086874a2bb45646e4f09653904ca
haeseok <haeseok_@naver.com>
2024-03-13 15:00:42 +0900

    회원 엔티티로 관심 프로젝트 조회하는 메서드 추가

--- src/main/java/kr/co/shoulf/web/repository/MoimLikeRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/MoimLikeRepository.java
@@ -1,8 +1,8 @@
 package kr.co.shoulf.web.repository;
 
-import kr.co.shoulf.web.entity.Meeting;
 import kr.co.shoulf.web.entity.Moim;
 import kr.co.shoulf.web.entity.MoimLike;
+import kr.co.shoulf.web.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +11,5 @@ import java.util.List;
 @Repository
 public interface MoimLikeRepository extends JpaRepository<MoimLike,Long> {
     List<MoimLike> findByMoim(Moim moim);
+    List<MoimLike> findByUsers(Users users);
 }
