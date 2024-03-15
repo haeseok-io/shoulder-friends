25bf5862f0f1f5041e210f04a018d30ccfc45dc0
TY <gkfnglfn@gmail.com>
2024-03-15 17:51:50 +0900

    로그인시 저장된 유저 정보 가져오기

--- src/main/java/kr/co/shoulf/web/control/AdminMainController.java
+++ src/main/java/kr/co/shoulf/web/control/AdminMainController.java
@@ -2,10 +2,12 @@ package kr.co.shoulf.web.control;
 
 import jakarta.servlet.http.HttpSession;
 import kr.co.shoulf.web.entity.Member;
+import kr.co.shoulf.web.security.custom.userDetails.CustomMemberUserDetails;
 import kr.co.shoulf.web.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,7 +24,8 @@ public class AdminMainController {
     private final MemberService memberService;
 
     @GetMapping("/")
-    public String adminMain(HttpSession session, Model model) {
+    public String adminMain(HttpSession session, Model model,
+                            @AuthenticationPrincipal CustomMemberUserDetails member) {
         // 현재 사용자의 인증 정보 가져오기
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("id", authentication.getName());
@@ -40,6 +43,7 @@ public class AdminMainController {
 
         session.setAttribute("loggedInUser", loggedInUser);
 
+//        System.out.println(member.getUsername());
         return "/admin/index";
     }
 }
