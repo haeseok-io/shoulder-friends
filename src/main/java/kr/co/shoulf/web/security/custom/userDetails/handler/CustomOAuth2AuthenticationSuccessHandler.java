0f61681facdd598348742c983cccee49abe05bcd
haeseok <haeseok_@naver.com>
2024-03-18 16:58:42 +0900

    일반 로그인 핸들러 추가

---
+++ src/main/java/kr/co/shoulf/web/security/custom/userDetails/handler/CustomOAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,34 @@
+package kr.co.shoulf.web.security.custom.userDetails.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import kr.co.shoulf.web.entity.Users;
+import kr.co.shoulf.web.security.auth.dto.CustomOAuth2User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomOAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
+        Users user = Users.builder()
+                .userNo(customOAuth2User.getUserNo())
+                .email(customOAuth2User.getEmail())
+                .build();
+
+        HttpSession session = request.getSession();
+        session.setAttribute("loggedInUser", user);
+
+        response.sendRedirect("/");
+    }
+}
