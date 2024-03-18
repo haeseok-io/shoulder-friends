ed4397d434c9a7d6d040b7b3606730ee81a73937
SJE <wodms1102@gmail.com>
2024-03-18 17:06:18 +0900

    모임번호랑 상태로 체크리스트 찾기 추가

--- src/main/java/kr/co/shoulf/web/repository/ChecklistRepository.java
+++ src/main/java/kr/co/shoulf/web/repository/ChecklistRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface ChecklistRepository extends JpaRepository<Checklist, Long> {
     //모임번호로 체크리스트 찾기
     List<Checklist> findByMoim_MoimNo(Long moimNo);
+    //모임번호랑 상태로 체크리스트 찾기
+    List<Checklist> findByMoim_MoimNoAndStatus(Long moimNo, int status);
 }
