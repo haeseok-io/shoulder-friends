900a1853d1e86e6766e134901678cd0704b80004
haeseok <haeseok_@naver.com>
2024-03-18 16:59:13 +0900

    userNo 멤버변수 추가

--- src/main/java/kr/co/shoulf/web/security/auth/dto/CustomOAuth2User.java
+++ src/main/java/kr/co/shoulf/web/security/auth/dto/CustomOAuth2User.java
@@ -1,5 +1,6 @@
 package kr.co.shoulf.web.security.auth.dto;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
@@ -10,10 +11,13 @@ import java.util.Map;
 public class CustomOAuth2User implements OAuth2User {
     private final OAuth2Response oAuth2Response;
     private final String role;
+    @Getter
+    private final Long userNo;
 
-    public CustomOAuth2User(OAuth2Response oAuth2Response, String role) {
+    public CustomOAuth2User(OAuth2Response oAuth2Response, String role, Long userNo) {
         this.oAuth2Response = oAuth2Response;
         this.role = role;
+        this.userNo = userNo;
     }
 
     @Override
@@ -43,4 +47,9 @@ public class CustomOAuth2User implements OAuth2User {
     public String getUsername() {
         return oAuth2Response.getProvider() + "_" + oAuth2Response.getProviderId();
     }
+
+    public String getEmail() {
+        return oAuth2Response.getEmail();
+    }
+
 }
