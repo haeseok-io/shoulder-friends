4658e11ee322f2acc048fae4523355a05328abe6
haeseok <haeseok_@naver.com>
2024-03-19 14:27:35 +0900

    로그인시 세션에 user 엔티티 저장되도록 수정

--- src/main/java/kr/co/shoulf/web/security/custom/userDetails/handler/CustomOAuth2AuthenticationSuccessHandler.java
+++ src/main/java/kr/co/shoulf/web/security/custom/userDetails/handler/CustomOAuth2AuthenticationSuccessHandler.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import kr.co.shoulf.web.entity.Users;
 import kr.co.shoulf.web.security.auth.dto.CustomOAuth2User;
+import kr.co.shoulf.web.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -16,18 +17,16 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class CustomOAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    private final UserService userService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
-        Users user = Users.builder()
-                .userNo(customOAuth2User.getUserNo())
-                .email(customOAuth2User.getEmail())
-                .build();
+        Users users = userService.readOne(customOAuth2User.getUserNo());
 
         HttpSession session = request.getSession();
-        session.setAttribute("loggedInUser", user);
+        session.setAttribute("loggedInUser", users);
 
         response.sendRedirect("/");
     }
