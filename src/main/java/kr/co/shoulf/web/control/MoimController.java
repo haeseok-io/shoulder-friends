0adde87d3d539759d2dc76c1e27476093f59ebac
YSM <ysm5545@naver.com>
2024-03-22 17:43:43 +0900

    모임 지원하기 기능 추가

--- src/main/java/kr/co/shoulf/web/control/MoimController.java
+++ src/main/java/kr/co/shoulf/web/control/MoimController.java
@@ -1,8 +1,8 @@
 package kr.co.shoulf.web.control;
 
 import jakarta.servlet.http.HttpSession;
-import jakarta.transaction.Transactional;
 import kr.co.shoulf.web.dto.*;
+import kr.co.shoulf.web.entity.MoimParticipants;
 import kr.co.shoulf.web.entity.Users;
 import kr.co.shoulf.web.service.*;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +26,7 @@ public class MoimController {
     private final PlatformService platformService;
     private final OnlineService onlineService;
     private final PositionService positionService;
+    private final UserJobService userJobService;
 
     @GetMapping(value = {"/", "/list"})
     public String list(PageRequestDTO pageRequestDto, Model model){
@@ -78,9 +79,28 @@ public class MoimController {
         return "redirect:/moim/";
     }
 
+    @PostMapping("/apply")
+    public String applyMoim(@RequestParam("positionDetailNo")Integer positionDetailNo,
+                          @RequestParam("moimHeadcountNo") Long moimHeadcountNo,
+                          @RequestParam("moimNo") Long moimNo,
+                          @ModelAttribute MoimParticipants moimParticipants,
+                          HttpSession session){
+        moimService.applyParticipant(positionDetailNo, moimHeadcountNo, session, moimParticipants);
+        System.out.println(moimParticipants);
+        return "redirect:/moim/detail?no=" + moimNo;
+    }
+
     @RequestMapping("/detail")
-    public String detail(@RequestParam("no") Long moimNo, @RequestParam(value = "type", required = false) String type, Model model, HttpSession session){
+    public String detail(@RequestParam("no") Long moimNo, @RequestParam(value = "type", required = false, defaultValue = "info") String type, Model model, HttpSession session){
         Users loggedInUser = (Users) session.getAttribute("loggedInUser");
+        /*if(loggedInUser != null) {
+            UserDTO userDTO = UserDTO.builder()
+                    .userNo(loggedInUser.getUserNo())
+                    .nickname(loggedInUser.getNickname())
+                    .build();
+            model.addAttribute("loggedInUser", userDTO);
+        }*/
+
         MoimDTO moimDTO = moimService.readOne(moimNo);
         String br = System.getProperty("line.separator").toString();
         model.addAttribute( "nlString", br);
@@ -88,14 +108,28 @@ public class MoimController {
         // 자물쇠 검증
         boolean ableControlTab;
         boolean ableMemberTab;
-        List<Users> moimMember = new ArrayList<>();
-        moimMember.add(moimDTO.getUsers());
+        List<UserDTO> moimMember = new ArrayList<>();
+        Users leader = moimDTO.getUsers();
+        // 리더 멤버에 추가
+        moimMember.add(UserDTO.builder()
+                 .userNo(leader.getUserNo())
+                 .nickname(leader.getNickname())
+                 .userDetail(leader.getUserDetail())
+                 .userJob(userJobService.readById(leader))
+                 .build());
         moimDTO.getHeadcountList().forEach(h->{ //모임 멤버 구해오기
             h.getParticipantApprovalList().forEach(p-> {
-                moimMember.add(p.getUsers());
+                moimMember.add(UserDTO.builder()
+                        .userNo(p.getUsers().getUserNo())
+                        .nickname(p.getUsers().getNickname())
+                        .userDetail(p.getUsers().getUserDetail())
+                        .userJob(userJobService.readById(p.getUsers()))
+                        .build());
             });
         });
         System.out.println(moimMember);
+
+        model.addAttribute("moimMember", moimMember);
         if(loggedInUser != null){
             //관리 탭
             ableControlTab = Objects.equals(loggedInUser.getUserNo(), moimDTO.getUsers().getUserNo());
@@ -104,8 +138,9 @@ public class MoimController {
             ableMemberTab = moimMember.stream().anyMatch(member -> Objects.equals(member.getUserNo(), loggedInUser.getUserNo()));
             model.addAttribute("ableMemberTab", ableMemberTab);
         }
-
+        // 탭 클릭별 보여줄 것
         if(type != null){
+            model.addAttribute("type", type);
             //정보 탭 클릭시
             if(type.equals("info")){
                 model.addAttribute("infoTab", true);
