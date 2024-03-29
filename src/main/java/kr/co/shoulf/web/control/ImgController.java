414706391bbea08c0e0fc54c86a94e55fdd98292
haeseok <haeseok_@naver.com>
2024-03-29 13:42:52 +0900

    EC2 서버에서 이미지 파일 불러오는 로직으로 변경

--- src/main/java/kr/co/shoulf/web/control/ImgController.java
+++ src/main/java/kr/co/shoulf/web/control/ImgController.java
@@ -27,8 +27,6 @@ public class ImgController {
             File file = new File(uploadPath + File.separator + srcFileName);
             HttpHeaders header = new HttpHeaders();
 
-            System.out.println(file);
-
             // Mime 타입 처리
             header.add("Content-Type", Files.probeContentType(file.toPath()));
 
